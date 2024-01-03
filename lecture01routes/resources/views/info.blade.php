9063cf1c30bc387fe1aa5b908fa31174a492debb
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-01-03 07:33:38 +0500

    final course commit

--- lecture01routes/resources/views/info.blade.php
+++ lecture01routes/resources/views/info.blade.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
 
 
-    <ul>
-        <li>Abdulrehman</li>
-        <li>Yousuf</li>
-        <li>Khuzaima</li>
-        <li>Yahya</li>
-        <li>Shoaiby</li>
-        <li>Saaria</li>
-        <li>Kashaf</li>
-        <li>KInza</li>
-        <li>Sheikh</li>
-        <li>Sana</li>
 
+    <h1>welcome {{ $id }}</h1>
+
+    {{-- <h2>Result= {{ $res}}</h2> --}}
+
+
+
+    <ul type="square">
+        <li>English = {{ $english }}</li>
+        <li>Urdu = {{ $urdu }}</li>
+        <li>Islamiyat = {{ $islamiyat }}</li>
+        <li>Obtained Marks = {{ $obtainedtotal }}</li>
+        <li>Percentage= {{ $per }}</li>
+        <li>Grade = {{ $grade }}</li>
     </ul>
 
 
 
 </body>
+
 </html>
--- lecture01routes/routes/web.php
+++ lecture01routes/routes/web.php
@@ -71,11 +71,65 @@ Route::get("/post/{id?}", function ($id = "no value here") {
 
 
 
-    echo"id ".$id;
-       // return view("mypost");
-//    })->whereNumber("id");
-//    })->whereAlpha("id");
-//    })->whereAlphaNumeric("id");
-//    })->where("id","[a-zA-Z0-9]+");
-   })->whereIn("id", ["khuzaima", "red", "Dulha"]);
+    echo "id " . $id;
+    // return view("mypost");
+    //    })->whereNumber("id");
+    //    })->whereAlpha("id");
+    //    })->whereAlphaNumeric("id");
+    //    })->where("id","[a-zA-Z0-9]+");
+})->whereIn("id", ["khuzaima", "red", "Dulha"]);
 
+
+
+
+
+
+
+Route::get("/home/{id?}/{english?}/{urdu?}/{islamiyat?}/{obtainedtotal?}/{per?}/{grade?}", function (
+
+    $id = "insert valid name ",
+    $english = "N/A",
+    $urdu = "N/A",
+    $islamiyat = "N/A",
+    $obtainedtotal = "N/A",
+    $per = "N/A",
+    $grade = "N/A",
+
+) {
+
+    $obtainedtotal = $english + $urdu + $islamiyat;
+
+
+
+
+    $per = ($obtainedtotal / 300) * 100;
+
+
+
+
+
+
+
+    return view("info", compact("id", "english", "urdu", "islamiyat", "obtainedtotal", "per", "grade"));
+});
+
+
+
+
+
+Route::get("/post/{id?}/{number?}/{res?}", function ($id = "no value here", $number = null, $res = null) {
+
+
+    if ($number % 2 == 0) {
+
+
+        $res = "even";
+    } else {
+        $res = "odd";
+    }
+
+
+
+
+    return view("info", compact("id", "res"));
+});
